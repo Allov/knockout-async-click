6db8580f8e0e107bdae57b5ba1ff2d73cadc3f6e
fcharest <frederik.charest@radio-canada.ca>
2016-05-27 14:55:50 -0400

    build tools

--- dist/async-click-binding-handler.js
+++ dist/async-click-binding-handler.js
@@ -1,93 +1,105 @@
-'use strict';
+(function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(['knockout', 'jquery', 'async-click-state'], factory);
+    } else if (typeof exports !== "undefined") {
+        factory(require('knockout'), require('jquery'), require('async-click-state'));
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(global.knockout, global.jquery, global.asyncClickState);
+        global.asyncClickBindingHandler = mod.exports;
+    }
+})(this, function (_knockout, _jquery, _asyncClickState) {
+    'use strict';
 
-var _knockout = require('knockout');
+    var _knockout2 = _interopRequireDefault(_knockout);
 
-var _knockout2 = _interopRequireDefault(_knockout);
+    var _jquery2 = _interopRequireDefault(_jquery);
 
-var _jquery = require('jquery');
+    var _asyncClickState2 = _interopRequireDefault(_asyncClickState);
 
-var _jquery2 = _interopRequireDefault(_jquery);
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }
 
-var _asyncClickState = require('async-click-state');
+    //var handlerId = 0;
 
-var _asyncClickState2 = _interopRequireDefault(_asyncClickState);
+    //TODO: (?) Suporter scénario comme https://github.com/knockout/knockout/blob/master/src/binding/defaultBindings/event.js
+    //http://knockoutjs.com/documentation/event-binding.html
+    _knockout2.default.bindingHandlers['asyncClick'] = {
+        'init': function init(element, valueAccessor, allBindings, viewModel, bindingContext) {
+            var handlerFunction = valueAccessor() || {};
+            var $element = (0, _jquery2.default)(element);
+            //var clickHandlerId = getClickHandlerId();
+            var clickHandlerId = 'click';
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+            _knockout2.default.applyBindingsToNode(element, {
+                safeClick: {}
+            });
 
-//var handlerId = 0;
+            $element.on(clickHandlerId, function (event) {
+                var handlerReturnValue;
 
-//TODO: (?) Suporter scénario comme https://github.com/knockout/knockout/blob/master/src/binding/defaultBindings/event.js
-//http://knockoutjs.com/documentation/event-binding.html
-_knockout2.default.bindingHandlers['asyncClick'] = {
-    'init': function init(element, valueAccessor, allBindings, viewModel, bindingContext) {
-        var handlerFunction = valueAccessor() || {};
-        var $element = (0, _jquery2.default)(element);
-        //var clickHandlerId = getClickHandlerId();
-        var clickHandlerId = 'click';
+                if (!handlerFunction || _asyncClickState2.default.asyncTask()) return;
 
-        _knockout2.default.applyBindingsToNode(element, {
-            safeClick: {}
-        });
+                try {
+                    // Take all the event args, and prefix with the viewmodel
+                    var argsForHandler = makeArray(arguments);
+                    viewModel = bindingContext['$data'];
+                    argsForHandler.unshift(viewModel);
+                    var originalHtml = $element.html();
+                    var asyncClickHtml = allBindings.get('asyncClickHtml');
 
-        $element.on(clickHandlerId, function (event) {
-            var handlerReturnValue;
-
-            if (!handlerFunction || _asyncClickState2.default.asyncTask()) return;
-
-            try {
-                // Take all the event args, and prefix with the viewmodel
-                var argsForHandler = makeArray(arguments);
-                viewModel = bindingContext['$data'];
-                argsForHandler.unshift(viewModel);
-                var originalHtml = $element.html();
-                var asyncClickHtml = allBindings.get('asyncClickHtml');
-
-                if (asyncClickHtml) {
-                    $element.html(asyncClickHtml);
-                }
-
-                handlerReturnValue = handlerFunction.apply(viewModel, argsForHandler);
-                _asyncClickState2.default.asyncTask(handlerReturnValue);
-
-                handlerReturnValue.always(function () {
                     if (asyncClickHtml) {
-                        $element.html(originalHtml);
+                        $element.html(asyncClickHtml);
                     }
 
-                    _asyncClickState2.default.asyncTask(null);
-                });
-            } finally {
-                //Par convention...
-                event.preventDefault();
-            }
+                    handlerReturnValue = handlerFunction.apply(viewModel, argsForHandler);
+                    _asyncClickState2.default.asyncTask(handlerReturnValue);
 
-            var bubble = allBindings.get('asyncClickBubble') !== false;
+                    handlerReturnValue.always(function () {
+                        if (asyncClickHtml) {
+                            $element.html(originalHtml);
+                        }
 
-            if (!bubble) {
-                event.cancelBubble = true;
-
-                if (event.stopPropagation) {
-                    event.stopPropagation();
+                        _asyncClickState2.default.asyncTask(null);
+                    });
+                } finally {
+                    //Par convention...
+                    event.preventDefault();
                 }
-            }
-        });
 
-        _knockout2.default.utils.domNodeDisposal.addDisposeCallback(element, function () {
-            $element.off(clickHandlerId);
-        });
+                var bubble = allBindings.get('asyncClickBubble') !== false;
+
+                if (!bubble) {
+                    event.cancelBubble = true;
+
+                    if (event.stopPropagation) {
+                        event.stopPropagation();
+                    }
+                }
+            });
+
+            _knockout2.default.utils.domNodeDisposal.addDisposeCallback(element, function () {
+                $element.off(clickHandlerId);
+            });
+        }
+    };
+
+    // function getClickHandlerId() {
+    //     return 'click.ko.clickHandler' + (++handlerId);
+    // }
+
+    function makeArray(arrayLikeObject) {
+        var result = [];
+
+        for (var i = 0, j = arrayLikeObject.length; i < j; i++) {
+            result.push(arrayLikeObject[i]);
+        }
+
+        return result;
     }
-};
-
-// function getClickHandlerId() {
-//     return 'click.ko.clickHandler' + (++handlerId);
-// }
-
-function makeArray(arrayLikeObject) {
-    var result = [];
-
-    for (var i = 0, j = arrayLikeObject.length; i < j; i++) {
-        result.push(arrayLikeObject[i]);
-    }
-
-    return result;
-}
+});
--- dist/async-click-state.js
+++ dist/async-click-state.js
@@ -1,32 +1,46 @@
-'use strict';
+(function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(['exports', 'jquery', 'knockout'], factory);
+    } else if (typeof exports !== "undefined") {
+        factory(exports, require('jquery'), require('knockout'));
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(mod.exports, global.jquery, global.knockout);
+        global.asyncClickState = mod.exports;
+    }
+})(this, function (exports, _jquery, _knockout) {
+    'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _jquery = require('jquery');
-
-var _jquery2 = _interopRequireDefault(_jquery);
-
-var _knockout = require('knockout');
-
-var _knockout2 = _interopRequireDefault(_knockout);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var AsyncClickState = function AsyncClickState() {
-    var self = this;
-
-    self.asyncTask = _knockout2.default.observable(null);
-
-    self.isBusy = _knockout2.default.pureComputed(function () {
-        return !!self.asyncTask();
+    Object.defineProperty(exports, "__esModule", {
+        value: true
     });
-};
 
-// AsyncClick.prototype.xyz = function() {
-//     var self = this;
+    var _jquery2 = _interopRequireDefault(_jquery);
 
-// };
+    var _knockout2 = _interopRequireDefault(_knockout);
 
-exports.default = new AsyncClickState();
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }
+
+    var AsyncClickState = function AsyncClickState() {
+        var self = this;
+
+        self.asyncTask = _knockout2.default.observable(null);
+
+        self.isBusy = _knockout2.default.pureComputed(function () {
+            return !!self.asyncTask();
+        });
+    };
+
+    // AsyncClick.prototype.xyz = function() {
+    //     var self = this;
+
+    // };
+
+    exports.default = new AsyncClickState();
+});
